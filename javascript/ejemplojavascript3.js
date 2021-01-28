b1efe1184aea28fa1641bc8356467db9981901c0
Diegolopez-ing <diegolopez344@gmail.com>
2021-01-28 10:37:59 -0500

    Funciones

---
+++ javascript/ejemplojavascript3.js
@@ -0,0 +1,31 @@
+function sumar(num1, num2){
+    console.log(arguments);
+    return num1+num2
+}
+
+const r=sumar(3,4)
+console.log(sumar(5,-3));
+console.log(r);
+console.log(sumar(r,5));
+
+/*DESTRUCTURACION*/
+
+function restar({num3, num4}){
+    console.log(arguments);
+    return num3-num4
+}
+
+const b=restar({num3:3,num4:4})
+console.log(b);
+
+
+
+function multiplicar(numeros){
+    console.log(arguments);
+    return num3-num4
+}
+
+const b=restar({num3:3,num4:4})
+console.log(b);
+
+
