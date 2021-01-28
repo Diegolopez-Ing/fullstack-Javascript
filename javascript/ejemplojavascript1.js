87ec1d30b1ddca1e16344e6bbe576c463ca4eb1b
Diegolopez-ing <diegolopez344@gmail.com>
2021-01-28 01:33:29 -0500

    Variables en Javascript

---
+++ javascript/ejemplojavascript1.js
@@ -0,0 +1,40 @@
+let numero1=1
+let numero2=2
+numero1+numero2
+let total = numero1+numero2
+total
+
+let string1='hola'
+let string2=' Diego'
+string1+string2
+let resultado=`${string1} ${string2}`
+resultado
+
+var numero=6
+numero='palabra'
+typeof numero
+
+const r =5
+typeof r
+let objeto1={propiedad1: 'texto1',
+             prop2:2,
+            prop3:{}}
+typeof objeto1
+objeto1.propiedad1
+objeto1.prop2
+objeto1.prop3
+typeof objeto1.propiedad1
+typeof objeto1.prop2
+typeof objeto1.prop3
+
+
+const diego=true
+typeof diego
+typeof Symbol()
+
+
+
+
+
+
+
