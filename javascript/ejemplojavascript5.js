4c39b4b65ff55609eeca59732683939fb668641f
Diegolopez-ing <diegolopez344@gmail.com>
2021-01-28 12:05:40 -0500

    Condicionales

---
+++ javascript/ejemplojavascript5.js
@@ -0,0 +1,33 @@
+const persona={
+    nombre: 'Diego',
+    apellido:'Lopez',
+    edad: 17,
+    id: 4
+}
+
+const persona2={
+    nombre: 'Alberto',
+    apellido:'LopMurilloez',
+    edad: 22,
+    id: 5
+}
+
+if (persona.edad>=18) {
+    console.log('Puedes tomar Cerveza');
+}
+else{
+    console.log('No puedes tomar cerrveza');
+}
+
+
+if (persona2.edad<=18) {
+    console.log('NO toma cerveza');
+    
+} else if(persona.nombre=='Diego'){
+    console.log('Canta');
+}
+else{
+    console.log('No canta y no toma cerveza');    
+}
+
+persona2.edad ==22
