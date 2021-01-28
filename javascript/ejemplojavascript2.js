ac09e184e750cded54b93e00f91de9ae4767a816
Diegolopez-ing <diegolopez344@gmail.com>
2021-01-28 10:19:29 -0500

    Objeto,Anidamiento,scope

---
+++ javascript/ejemplojavascript2.js
@@ -0,0 +1,34 @@
+let persona={
+    nombre: 'Diego L',
+    apellido: 'Lopez M',
+    email: 'diegolopez344@gmail.com'
+}
+
+persona.apellido
+persona.nombre
+persona.email
+
+
+
+
+
+/*Anidamiento*/
+
+var z= 'primera variable'
+let x='y'
+
+{
+    let i='INformacion'
+    let u= 'otra cosa'
+    {
+        const r=3
+        var j=18
+        {
+            u=5
+        }
+    }
+}
+console.log(x);
+console.log(i);
+console.log(j);
+console.log(u);
