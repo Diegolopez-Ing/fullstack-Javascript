8d06ca8001f218e55e5ccb9162009e6d5b941a0c
Diegolopez-ing <diegolopez.coder@gmail.com>
2021-02-03 21:08:49 -0500

    Adicion de propietarios

--- veterinaria/js/mascotas.js
+++ veterinaria/js/mascotas.js
@@ -1,4 +1,9 @@
 const listaMascotas=document.getElementById('lista-mascotas')
+const nombre=document.getElementById('nombre')
+const tipo=document.getElementById('tipo')
+const dueno=document.getElementById('dueno')
+const form=document.getElementById('form')
+const btnGuardar=document.getElementById('btn-guardar')
 
 let mascotas = [
     {
@@ -9,7 +14,7 @@ let mascotas = [
     {
         tipo: 'Gato',
         nombre: 'Manchas',
-        dueno: 'Diego'
+        dueno: 'Maria'
     },
     {
         tipo: 'Gato',
@@ -34,4 +39,20 @@ function listarMascotas() {
     <tr>`).join('')
     listaMascotas.innerHTML = htmlMascotas
 }
+
+function enviarDatos(evt) {
+    console.log('evento',evt);
+    const datos={
+        tipo: tipo.value,
+        nombre: nombre.value,
+        dueno: dueno.value        
+    }
+    mascotas.push(datos)
+    listarMascotas()
+}
+
 listarMascotas()
+
+form.onsubmit = enviarDatos
+btnGuardar.onclick= enviarDatos
+
