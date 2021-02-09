2e87283d31712e61d64fa26204bf1bb6e8b80791
Diegolopez-ing <diegolopez.coder@gmail.com>
2021-02-09 15:47:32 -0500

    Obtener stream del payload e unir sus pedazos con un solo string

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,42 +1,58 @@
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder
+
 
 const server = http.createServer((req, res) => {
-    const urlActual= req.url
-    const urlParseada= url.parse(urlActual,true)
+    const urlActual = req.url
+    const urlParseada = url.parse(urlActual, true)
 
-        //2. Obtener la ruta
-    const ruta=urlParseada.pathname
+    //2. Obtener la ruta
+    const ruta = urlParseada.pathname
 
     // 3.Quitar slash
-    const rutaLimpia=ruta.replace(/^\/+|\/+$/g,'')
+    const rutaLimpia = ruta.replace(/^\/+|\/+$/g, '')
 
-// 3.1 Método HTTP
-    const method=req.method.toLowerCase();
+    // 3.1 Método HTTP
+    const method = req.method.toLowerCase();
 
-    
+
     // 3.2 Obtener Variables del query url parseado
     // console.log({urlParseada});
-    const { query ={} }= urlParseada
-    console.log({query});
+    const { query = {} } = urlParseada
+    console.log({ query });
 
     // 3.3 Obtener los headers 
-    const { headers} =req
+    const { headers } = req
     console.log(headers);
 
+    // 3.4 Obtener payload en el caso de haberlo
+    const decoder = new StringDecoder('utf-8')
+    let buffer = ''
+
+    //3.4.1 Ir acumulando la data cuando request reciba un oayload
+    req.on('data', (data) => {
+        buffer += decoder.write(data)
+    })
+
+    //3.4.2 Dejar de acumular Datos y decirle al decoder que finalice
+    req.on('end', () => {
+        buffer += decoder.end()
+    })
 
     //4. enviar una respuesta dependiendo de la ruta
     switch (rutaLimpia) {
         case ruta:
             res.end('Esta es una ruta conocida')
-            break;    
+            break;
+            
         default:
             res.end('desconocida')
     }
-    
+
 });
 
-server.listen(5000,()=>{
+server.listen(5000, () => {
     console.log('...El servidor esta escuchando peticionnes en la url http://localhost:5000/');
 });
 
