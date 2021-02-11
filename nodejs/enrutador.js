2d6fb7f22167169213e9c3ad8c1a448b3807518e
Diegolopez-ing <diegolopez.coder@gmail.com>
2021-02-11 01:06:29 -0500

    Actualizar mascotas

--- nodejs/enrutador.js
+++ nodejs/enrutador.js
@@ -15,6 +15,16 @@
         post:(data, callback) => {
             global.recursos.mascotas.push(data.payload)
             callback(201,data.payload)
+        },
+        put:(data, callback) => {
+            if (typeof data.indice !== "undefined") {
+                if (global.recursos.mascotas[data.indice]) {
+                    global.recursos.mascotas[data.indice]=data.payload
+                    return callback(200,global.recursos.mascotas[data.indice])
+                }
+                return callback(404,{mensaje:`Mascota con indice ${data.indice} no encontrado`}) 
+            }
+            callback(400,{mensaje:"Indice no enviado"})
         }
     },
     noEncontrado: (data, callback) => {
