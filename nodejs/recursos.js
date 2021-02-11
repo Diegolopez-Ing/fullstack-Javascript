ad93fa1febf2de55e6080218625e7ae7fb57d5cd
Diegolopez-ing <diegolopez.coder@gmail.com>
2021-02-11 14:49:39 -0500

    Ruta y handlers para veterinarias

--- nodejs/recursos.js
+++ nodejs/recursos.js
@@ -4,5 +4,12 @@ module.exports={
         { tipo: "perro",nombre:"Toby",dueno:"Andres"},
         { tipo: "Loro",nombre:"Lazzy",dueno:"Mario"},
         { tipo: "perro",nombre:"Moncho",dueno:"Juliana"},
+    ],
+    veterinarias:[
+        { nombre: "Andrea",apellido:"Padierna",documento:"1234244"},
+        { nombre: "Alexander",apellido:"Rovira",documento:"76879"},
+        { nombre: "Susana",apellido:"Rua",documento:"998999"},
+        { nombre: "Reinaldo",apellido:"Rueda",documento:"2021"},
+       
     ]
 }
