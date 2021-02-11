7604a7361b843b41ce8323bf72fec463c251fead
Diegolopez-ing <diegolopez.coder@gmail.com>
2021-02-11 13:16:56 -0500

    Ordenar Handlers de ruta en carperta Ruta

--- nodejs/enrutador.js
+++ nodejs/enrutador.js
@@ -1,43 +1,10 @@
+    const recursos=require("./recursos")
+    const mascotas=require("./ruta/mascota")
     module.exports= {        
     ruta: (data, callback) => {
         callback(200, { mensaje: "Esta es /ruta" })
     },
-    mascotas: {
-        get:(data, callback) => {
-            if (typeof data.indice !== "undefined") {
-                if (global.recursos.mascotas[data.indice]) {
-                    return callback(200,global.recursos.mascotas[data.indice])
-                }
-                return callback(404,{mensaje:`Mascota con indice ${data.indice} no encontrado`}) 
-            }
-            callback(200,global.recursos.mascotas)
-        },
-        post:(data, callback) => {
-            global.recursos.mascotas.push(data.payload)
-            callback(201,data.payload)
-        },
-        put:(data, callback) => {
-            if (typeof data.indice !== "undefined") {
-                if (global.recursos.mascotas[data.indice]) {
-                    global.recursos.mascotas[data.indice]=data.payload
-                    return callback(200,global.recursos.mascotas[data.indice])
-                }
-                return callback(404,{mensaje:`Mascota con indice ${data.indice} no encontrado`}) 
-            }
-            callback(400,{mensaje:"Indice no enviado"})
-        },
-        delete:(data, callback) => {
-            if (typeof data.indice !== "undefined") {
-                if (global.recursos.mascotas[data.indice]) {
-                    global.recursos.mascotas=global.recursos.mascotas.filter(
-                        (_mascota, indice)=>indice!= data.indice)
-                    return callback(204,{mensaje: `elemento con indice ${data.indice} eliminado`})
-                }
-                return callback(404,{mensaje:`Mascota con indice ${data.indice} no encontrado`}) 
-            }
-            callback(400,{mensaje:"Indice no enviado"})
-        }
-    },
+    mascotas: mascotas(recursos.mascotas),
     noEncontrado: (data, callback) => {
         callback(404, { mensaje: "no encontrado" })
     }
