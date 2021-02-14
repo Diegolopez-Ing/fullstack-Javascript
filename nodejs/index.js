b034a8441c7879130e60c1be94315a81898137a4
Diegolopez-ing <diegolopez.coder@gmail.com>
2021-02-14 12:31:00 -0500

    Reemplazo URL

--- nodejs/index.js
+++ nodejs/index.js
@@ -5,7 +5,7 @@ const requestHandler=require("./request-handler")
 const server=http.createServer(requestHandler)
 
 server.listen(5000, () => {
-    console.log("El servidor esta escuchando peticionnes en la url http://localhost:5000/");
+    console.log("El servidor esta escuchando peticionnes en la url https://veterinaria-backend-delta.vercel.app/");
 });
 
 
